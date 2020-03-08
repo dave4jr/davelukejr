98ba17a6f7e435715c153497e7ce5a411e695fd2
dave4jr <dave@getcairn.com>
2020-03-07 17:20:14 -0800

    commiting recent patches and updates to portfolio moving letters site

--- src/components/App.js
+++ src/components/App.js
@@ -22,14 +22,21 @@ class App extends React.Component {
 				<canvas className="color-overlay" id="color-overlay"></canvas>
 				<Header />
 				<div className="compositions">
-					<Composition effect="effect8" circle="#335062" background="#A5D1DA" color="#fff" text="Hi"/>
-					<Composition effect="effect4" background="#E0D5BB" color="#fff" text="My Name Is Dave"/>
+					<Composition effect="effect8" circle="#335062" background="#A5D1DA" color="#fff" text="Work"/>
+					<Composition effect="effect11" background="#E0D5BB" color="#fff" text="I Am Dave"/>
 					<Composition effect="effect13" background="#3D5742" color="#fff" text="I Am Dave"/>
 					<Composition effect="effect1" background="#DF7360" color="#fff" text="I Am Dave"/>
-					<Composition effect="effect5" background="#335062" color="#fff" text="Wood Metal"/>
-					<Composition effect="effect11" background="#E5B7B7" color="#fff" text="Hello"/>
-					<Composition effect="effect6" background="#A5D1DA" color="#fff" text="Dave Luke Jr"/>
+					<Composition effect="effect5" background="#335062" color="#fff" text="I Am Dave"/>
+					<Composition effect="effect4" background="#E5B7B7" color="#fff" text="I Am Dave"/>
+					<Composition effect="effect6" background="#A5D1DA" color="#fff" text="I Am Dave"/>
 					<Composition effect="effect15" background="#C7C1BB" color="#fff" text="I Am Dave"/>
+					<Composition effect="effect2" background="#3D5742" color="#fff" text="I Am Dave"/>
+					<Composition effect="effect3" background="#3D5742" color="#fff" text="I Am Dave"/>
+					<Composition effect="effect7" background="#A5D1DA" color="#fff" text="I Am Dave"/>
+					<Composition effect="effect9" background="#335062" color="#fff" text="I Am Dave"/>
+					<Composition effect="effect10" background="#E0D5BB" color="#fff" text="I Am Dave"/>
+					<Composition effect="effect12" background="#DF7360" color="#fff" text="I Am Dave"/>
+					<Composition effect="effect14" background="#E5B7B7" color="#fff" text="I Am Dave"/>
 				</div>
 			</div>
 		)
